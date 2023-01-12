d93c524a3cbb1648cc23c0080647bb7f363fcc5b
amrit <info.amritpant@gmail.com>
2023-01-12 23:16:07 +0545

    Project Finished

---
+++ script.js
@@ -0,0 +1,67 @@
+const generateButton = document.querySelector(".btn-main");
+const sectionGenerateButton = document.querySelector(
+  ".section__generate-letter"
+);
+const showButton = document.querySelector(".btn-secondary");
+const headingSecondary = document.querySelector(".heading-secondary");
+const sectionTable = document.querySelector(".section__table");
+const placePara = document.querySelector("#place");
+const tryPara = document.querySelector("#try");
+const vibrationPara = document.querySelector("#vibration");
+const powerPara = document.querySelector("#power");
+
+let state = 0; //  0 -> Normal State
+// 1 -> Generated but not shown
+// 2 -> shown and generated
+let randomValue;
+
+generateButton.addEventListener("click", () => {
+  if (state === 0) {
+    randomValue = Math.trunc(Math.random() * 29);
+    console.log(randomValue);
+    const letter = phonemeArr[randomValue]?.letter;
+    state = 1;
+    headingSecondary.textContent = letter;
+    generateButton.innerHTML = "Cancel";
+    showButton.style.display = "inline";
+  } else if (state === 1) {
+    headingSecondary.textContent = "";
+    generateButton.innerHTML = "Generate";
+    state = 0;
+    showButton.style.display = "none";
+  }
+});
+
+showButton.addEventListener("click", () => {
+  if (state == 1) {
+    sectionTable.style.display = "block";
+    const letterObj = phonemeArr[randomValue];
+
+    phonemeDetails.forEach((el) => {
+      if (el[0] === letterObj.place) {
+        placePara.textContent = placePara.textContent + el[1];
+      }
+      if (el[0] === letterObj.try) {
+        tryPara.textContent = tryPara.textContent + el[1];
+      }
+      if (el[0] === letterObj.power) {
+        powerPara.textContent = powerPara.textContent + el[1];
+      }
+      if (el[0] === letterObj.vibration) {
+        vibrationPara.textContent = vibrationPara.textContent + el[1];
+      }
+    });
+    console.log(letterObj);
+
+    state = 2;
+    showButton.textContent = "hide";
+  } else if (state == 2) {
+    state = 1;
+    sectionTable.style.display = "none";
+    showButton.textContent = "show";
+    placePara.textContent = "स्थानका आघारमा: ";
+    tryPara.textContent = "प्र्यतनका आघारम: ";
+    vibrationPara.textContent = "घोषत्वका आघारम: ";
+    powerPara.textContent = "पारणत्वका आघारम: ";
+  }
+});
